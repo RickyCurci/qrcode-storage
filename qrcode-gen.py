abf86f67bff8d9dc73d2e0c7844f2f24d09a0975
RickyCurci <ric.cur@yahoo.com>
2022-06-25 18:31:38 +0200

    initial commit

---
+++ qrcode-gen.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+from pyzbar.pyzbar import decode
+from PIL import Image
+import qrcode
+import sys
+import os
+
+
+
+def encoding(name): 
+	f = name; f = open(f,"r"); string = ""	
+	for i in f: 
+		string = string+"\n"+i
+	ASCII_values = []; values = []; value = []
+	for character in string:
+		ASCII_values.append(ord(character))
+	for i in range(0,len(ASCII_values)):
+		for j in range(0,8): 
+			if ASCII_values[i]%2 != 0: 
+				value.append("1")
+				ASCII_values[i] = int(ASCII_values[i]/2)
+			else:
+				value.append("0")
+				ASCII_values[i] = int(ASCII_values[i]/2)
+
+		values.append(value[::-1]); value = []; 
+	string = ""
+	for i in values: 
+		string += str("".join(i))
+	print(string)
+	
+	if len(string) <= 2953:
+		qr = qrcode.QRCode(
+
+				version=40,
+				error_correction=qrcode.constants.ERROR_CORRECT_L,
+		)
+
+		img = qr.make_image(qr.add_data(string))
+		img.save(name+".jpg"); os.system("mv "+name+".jpg"+" src/")
+
+		print("open > '"+name+".jpg'")
+
+	elif len(string) > 2960: 
+
+
+
+		nstring = string; point = 0
+		print(len(nstring))
+		for i in range(0,round(len(nstring)//2960)+1):
+			qr = qrcode.QRCode(
+
+					version=40,
+					error_correction=qrcode.constants.ERROR_CORRECT_L,
+			)
+			img = qr.make_image(qr.add_data(nstring[point:(point+2960)])); point += 2960
+			img.save(name+str(i)+".jpg"); os.system("mv "+name+str(i)+".jpg"+" src/")
+			print("open > '"+name+str(i)+".jpg'")
+
+
+def decoe(file): 	
+	d = decode(Image.open(file))
+	
+	string = d[0].data.decode("ascii");  
+	string = list(string); sub_string=[]; rstring = []
+	for i in range(0,int(len(string)/8)): 
+		for j in range(0,8): 
+			sub_string.append(string[0]); string.remove(string[0]);
+		rstring.append(int("".join(sub_string),2)); sub_string = []
+	print(''.join(chr(i) for i in rstring))
+
+
+
+if sys.argv[1] == "-e":
+	encoding(sys.argv[2])
+elif sys.argv[1] == "-d": 
+	decoe(sys.argv[2])
